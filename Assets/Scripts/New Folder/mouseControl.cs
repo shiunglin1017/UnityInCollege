4a4d5b0990a3182ca15cf781f72bd14479ae16d7
shiunglin1017 <martin19991017@gmail.com>
2021-06-17 16:46:25 +0800

    將鼠標轉入專案，但沒辦法移動很好
    使用prefab裡面的mouse來放進去，然後讓他只有在標題時能跑一些

---
+++ Assets/Scripts/New Folder/mouseControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseControl : MonoBehaviour
+{
+    [SerializeField]Camera camerafor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var mouseScript = new PlayBandMouse();
+        mouseScript.mouseCamera = camerafor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
